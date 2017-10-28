9027618a04186f2234b348c6c2e3f052d1fdcacc
Webdevmatics <webdevmatics@gmail.com>
2017-10-29 03:45:16 +0545

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,11 +16,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+         Schema::defaultStringLength(191);
+       
         if (Schema::hasTable('tags')) 
         {
-         Schema::defaultStringLength(191);
-
-         View::share('tags', Tag::all());
+             View::share('tags', Tag::all());
         }
     }
 
